64a5d99101cca761096678df8072555c8367bb4e
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-08-29 22:38:18 +0000

    Time: 780 ms (56.83%) | Memory: 17 MB (38.61%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,17 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        mem = [-1]*(amount + 1)
+        mem = [amount + 1]*(amount + 1)
         mem[0] = 0
-        for i in range(0, amount + 1):
-            if mem[i] == -1:
-                continue
-            for coin in coins:
-                if i + coin > amount:
-                    continue
-
-                if mem[i+coin] == -1:
-                    mem[i+coin] = mem[i] + 1
-                else:
-                    mem[i+coin] = min(mem[i+coin], mem[i] + 1)
-        return mem[amount]
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >=0:
+                    mem[i] = min(mem[i], mem[i - c] + 1)
+        return mem[amount] if mem[amount] != (amount + 1) else -1
         
