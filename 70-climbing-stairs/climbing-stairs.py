1f6a4589d76f8285e4a921c24daface3f5af85af
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-08-29 21:18:35 +0000

    Time: 34 ms (59.35%) | Memory: 16.5 MB (14.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,22 +3,12 @@ class Solution:
     def __init__(self) -> None:
         self.mem = [-1]
 
-    def topDown(self, n) -> int:
-        count = 0
-        if n < 0:
-            return 0
-        if n == 0:
-            return 1
-        if n == 1:
-            return 2
-        if self.mem[n] != -1:
-            return self.mem[n]
-        count += self.topDown(n-1)
-        count += self.topDown(n-2)
-
-        self.mem[n] = count
-        return count
-
     def climbStairs(self, n: int) -> int:
         self.mem = [-1] * n
-        return self.topDown(n-1)
+        self.mem[0] = 1
+        if n == 1:
+            return 1
+        self.mem[1] = 2
+        for i in range(2, n):
+            self.mem[i] = self.mem[i-1] + self.mem[i-2]
+        return self.mem[n-1]
