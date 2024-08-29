b80f0e68093bc76973dfd24ee9ac913705b4e62d
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-08-29 22:50:32 +0000

    Time: 1312 ms (9.52%) | Memory: 19.3 MB (25.59%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,10 +1,24 @@
 class Solution:
+    def __init__(self) -> None:
+        self.mem = []
+
+    def topDown(self, coins: List[int], amount: int):
+        if amount == 0:
+            return 0
+
+        if self.mem[amount] != -2:
+            return self.mem[amount]
+
+        sol = sys.maxsize
+        for c in coins:
+            if amount - c >= 0:
+                val = self.topDown(coins, amount - c)
+                if val == -1:
+                     continue
+                sol = min(sol, 1 + self.topDown(coins, amount - c))
+        self.mem[amount] = sol if sol != sys.maxsize else -1
+        return self.mem[amount]
+
     def coinChange(self, coins: List[int], amount: int) -> int:
-        mem = [amount + 1]*(amount + 1)
-        mem[0] = 0
-        for i in range(1, amount + 1):
-            for c in coins:
-                if i - c >=0:
-                    mem[i] = min(mem[i], mem[i - c] + 1)
-        return mem[amount] if mem[amount] != (amount + 1) else -1
-        
+        self.mem = [-2]*(amount + 1)
+        return self.topDown(coins, amount)
