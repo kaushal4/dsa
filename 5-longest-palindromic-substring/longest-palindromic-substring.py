6ac04a3b8bf1077cd94698f6cebf2da04f766aa5
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-08-30 00:45:34 +0000

    Time: 1643 ms (37.51%) | Memory: 16.4 MB (92.82%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -5,24 +5,34 @@ class Solution:
             return 0
         sol = 1
         sol_values = s[0]
-        mem = [[-1] * n for _ in range(n)]
 
-        for i in range(n):
-            mem[i][i] = 1
+        # check odd
+        mem = [1] * n
 
-        for i in range(1, n):
+        for i in range(2, n, 2):
             for j in range(i, n):
                 k = j - i
-                if s[j] != s[k]:
-                    mem[j][k] = -1 
-                    continue
-                if j-1 < k + 1:
-                    mem[j][k] = 2
-                elif (mem[j-1][k+1] != -1):
-                    mem[j][k] = max(mem[j][k], 2 + mem[j-1][k+1])
+                if s[j] == s[k] and mem[k+1] != -1:
+                    mem[k] = 2 + mem[k+1]
+                    if sol < mem[k]:
+                        sol = mem[k]
+                        sol_values = s[k:j+1]
+                else:
+                    mem[k] = -1
 
-                if mem[j][k] > sol:
-                    sol = mem[j][k]
-                    sol_values = s[k:j+1]
-        
+        # check even
+        mem = [0] * n 
+
+        for i in range(1, n, 2):
+            for j in range(i, n):
+                k = j - i
+                if s[j] == s[k] and mem[k+1] != -1:
+                    mem[k] = 2 + mem[k+1]
+                    if sol < mem[k]:
+                        sol = mem[k]
+                        sol_values = s[k:j+1]
+                else:
+                    mem[k] = -1
+
+
         return sol_values
