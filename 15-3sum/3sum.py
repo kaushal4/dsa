991625a2289affdd1418a3995203eb8ff57f37c0
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-09-03 22:51:33 +0000

    Time: 721 ms (44.67%) | Memory: 19.9 MB (95.12%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,9 +3,11 @@ class Solution:
         nums.sort()
         n = len(nums)
         sol = list()
-        visited = set()
         
         for first in range(0, n-2):
+            # condition - 1 to avoid duplicates
+            if first > 0 and nums[first] == nums[first-1]:
+                continue
             second = first + 1
             third = n - 1
 
@@ -13,12 +15,16 @@ class Solution:
                 sum = nums[first] + nums[second] + nums[third]
                 if sum == 0:
                     sol.append((nums[first], nums[second], nums[third]))
-                    second += 1
+                    cur_second = second
+                    cur_third = third
+                    # condition - 2 to avoid duplicates
+                    while second < n-1 and nums[cur_second] == nums[second]:
+                        second += 1
+                    # condition - 2 to avoid duplicates
+                    while third > second and nums[cur_third] == nums[third]:
+                        third -= 1
                 elif (sum < 0):
                     second += 1
                 else:
                     third -= 1
-
-        
-
-        return list(set(sol))
+        return sol
