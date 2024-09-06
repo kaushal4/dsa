52c9d41782d138ec5de60324de0e8c4a7b267889
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-09-06 22:00:37 +0000

    Time: 328 ms (5.26%) | Memory: 16.6 MB (39.12%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,37 @@
+class Solution:
+    def __init__(self) -> None:
+        self.hash = dict()
+
+    def add(self, c:str):
+        if c in self.hash:
+            self.hash[c] += 1
+        else:
+            self.hash[c] = 1
+
+
+    def remove(self, c:str):
+        if c in self.hash and self.hash[c] > 1:
+            self.hash[c] -= 1
+        else:
+            self.hash.pop(c)
+
+    def maxChar(self) -> int:
+        maxChar = -1
+        for c in self.hash:
+            maxChar = max(maxChar, self.hash[c])
+        return maxChar
+
+    def characterReplacement(self, s: str, k: int) -> int:
+        low = -1
+        high = -1
+        sol = 0
+
+        while high < len(s) - 1:
+            high = high + 1
+            self.add(s[high])
+            while (high - low) > (self.maxChar() + k):
+                low += 1
+                self.remove(s[low])
+            sol = max(sol, high - low)
+
+        return sol
