3123e69d5ed620aa242d048db61b9e0b5e899d20
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-09-09 20:08:53 +0000

    Time: 33 ms (84.65%) | Memory: 17.5 MB (45.89%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -4,23 +4,29 @@ class Solution:
             return None
         
         head_pointer = Node(-1, head)
-        duplicates = []
 
         while head != None:
             copy = Node(head.val, None)
-            duplicates.append(copy)
             copy.next = head.next 
             head.next = copy
             head = copy.next
 
         head = head_pointer.next
+        sol_pointer = Node(-1, head.next)
 
         while head != None and head.next != None:
             if head.random != None:
                 head.next.random = head.random.next
             head = head.next.next
 
-        for i in range(0, len(duplicates)):
-            duplicates[i].next = duplicates[i + 1] if i + 1 < len(duplicates) else None
+        head = head_pointer.next
 
-        return duplicates[0]
+        while head != None:
+            temp = head.next.next
+            if temp != None:
+                head.next.next = temp.next
+            else:
+                head.next.next = None
+            head = temp
+
+        return sol_pointer.next
