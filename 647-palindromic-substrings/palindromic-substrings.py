072ced4819a46622a9f532e552ff7daae46e923a
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-09-11 23:39:42 +0000

    Time: 202 ms (37.78%) | Memory: 16.5 MB (79.39%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,41 +1,27 @@
 class Solution:
-    def countSubstrings(self, s: str) -> str:
+    def countSubstrings(self, s: str) -> int:
         n = len(s)
-        if n < 0:
-            return 0
-        sol = 1
-        sol_values = s[0]
-        count = n
+        sol = n
 
-        # check odd
-        mem = [1] * n
-
-        for i in range(2, n, 2):
+        #odd
+        dp = [True] * n
+        for i in range(2,n,2):
             for j in range(i, n):
-                k = j - i
-                if s[j] == s[k] and mem[k+1] != -1:
-                    count += 1
-                    mem[k] = 2 + mem[k+1]
-                    if sol < mem[k]:
-                        sol = mem[k]
-                        sol_values = s[k:j+1]
+                if s[j] == s[j-i] and dp[j-i+1]:
+                    dp[j-i] = True
+                    sol+= 1
                 else:
-                    mem[k] = -1
+                    dp[j-i] = False
+        
+        #Even
+        dp = [True] * n
 
-        # check even
-        mem = [0] * n 
-
-        for i in range(1, n, 2):
+        for i in range(1,n,2):
             for j in range(i, n):
-                k = j - i
-                if s[j] == s[k] and mem[k+1] != -1:
-                    count += 1
-                    mem[k] = 2 + mem[k+1]
-                    if sol < mem[k]:
-                        sol = mem[k]
-                        sol_values = s[k:j+1]
+                if s[j] == s[j-i] and dp[j-i+1]:
+                    dp[j-i] = True
+                    sol+= 1
                 else:
-                    mem[k] = -1
-
-
-        return count
+                    dp[j-i] = False
+        
+        return sol
