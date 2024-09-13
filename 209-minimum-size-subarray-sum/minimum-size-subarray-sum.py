43a96d0ed937e601ed1fac55130917a24b44ef6e
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-09-13 03:55:37 +0000

    Time: 236 ms (5.05%) | Memory: 30.4 MB (20.28%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getVal(self, nums:List[int], index:int) -> int:
+        if index == -1:
+            return 0
+        return nums[index]
+
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        low = -1
+        high = 0
+        sol = n
+        if target > sum(nums):
+            return 0
+        for i in range(0, n):
+            nums[i] = nums[i] + self.getVal(nums, i -1)
+        
+        while high < n:
+            subSum = self.getVal(nums, high) - self.getVal(nums, low)
+            if subSum < target:
+                high += 1
+            else:
+                sol = min(sol, high - low)
+                low += 1
+                if low == high:
+                    return 1
+        
+        return sol
