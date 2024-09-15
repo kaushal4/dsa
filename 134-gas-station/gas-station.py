61b8678247f4bc48903bddf03f0c0ad14b55c1ed
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-09-15 05:19:41 +0000

    Time: 769 ms (95.57%) | Memory: 22 MB (86.13%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,16 +1,12 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
-        diff = [gas[i] - cost[i] for i in range(0, n)]
-        cum = sum(diff)
-        if cum < 0 :
-            return -1
         index = n
         tank = 0
         max_tank = 0
         for i in range(n-1, -1, -1):
-            tank = tank + diff[i]
+            tank = tank + gas[i] - cost[i]
             if tank >= max_tank:
                 index = i
                 max_tank = tank
-        return index
+        return index if tank >= 0 else -1
