9fec71dd921af8a166185af3a68b03f460ea5ff3
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-10-13 03:52:34 +0000

    Time: 1119 ms (5.27%) | Memory: 16.9 MB (97.25%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.py
@@ -0,0 +1,48 @@
+class Solution:
+    def findMax(self, freqs, intervals):
+        max_key = '.'
+        max_key_val = 0
+        for key in freqs:
+            if freqs[key] > max_key_val and intervals[key] == 0:
+                max_key_val = freqs[key]
+                max_key = key
+        return max_key
+
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        t_len = len(tasks)
+        freqs = dict()
+        intervals = dict()
+        sol = 0
+        for t in tasks:
+            if t in freqs:
+                freqs[t] += 1
+            else:
+                freqs[t] = 1
+                intervals[t] = 0
+
+        while(t_len > 0):
+            t_len-= 1
+
+            # wait for some time if no task is ready
+            cheapest = min(intervals.values())
+            for key in intervals:
+                intervals[key] -= cheapest
+            sol += cheapest
+
+            # select task to do
+            task_to_do = self.findMax(freqs=freqs, intervals=intervals) 
+
+            # run the task
+            freqs[task_to_do] -= 1
+            for key in intervals:
+                intervals[key] = max(0, intervals[key] - 1)
+            intervals[task_to_do] = n
+            sol += 1
+
+            # remove the currtly run element if it is fully processed 
+            if freqs[task_to_do] == 0:
+                freqs.pop(task_to_do)
+                intervals.pop(task_to_do)
+
+
+        return sol
