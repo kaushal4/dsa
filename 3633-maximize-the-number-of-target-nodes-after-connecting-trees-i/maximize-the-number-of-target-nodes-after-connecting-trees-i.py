ddb03d6853daa01d19cbc33d0ca23a5313dd82a6
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-12-16 22:02:00 +0000

    Time: 1913 ms (63.10%) | Memory: 18.6 MB (23.05%) - LeetSync

---
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.py
@@ -0,0 +1,48 @@
+class Solution:
+    def bfs(self, adj:Dict[int, List[int]], source:int, k:int) -> int:
+        if k < 0:
+            return 0
+        queue:Deque[int] = deque()
+        queue.append(source)
+        visited = set()
+        visited.add(source)
+        sol = 1
+        while k and queue:
+            b = len(queue)
+            while(b):
+                node = queue.popleft()
+                for child in adj[node]:
+                    if child not in visited:
+                        visited.add(child)
+                        queue.append(child)
+                        sol += 1
+                b-= 1
+            k -= 1
+        return sol
+
+    def getDist(self, adj:Dict[int, List[int]], k:int) -> List[int]:
+        sol = []
+        for i in range(0, len(adj.keys())):
+            val = self.bfs(adj, i, k)
+            sol.append(val)
+        return sol
+
+    def createAdj(self, edges:List[List[int]]):
+        n = len(edges)
+        adj = {i:[] for i in range(n + 1)}
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+        return adj
+
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
+        adj1 = self.createAdj(edges1)
+        adj2 = self.createAdj(edges2)
+        dist2 = self.getDist(adj2, k-1)
+        dist1 = self.getDist(adj1, k)
+        best = max(dist2)
+        print(dist1)
+        print(dist2)
+        for i in range(len(dist1)):
+            dist1[i] += best
+        return dist1
