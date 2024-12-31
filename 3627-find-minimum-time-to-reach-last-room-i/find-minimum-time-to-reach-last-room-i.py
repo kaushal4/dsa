5f6132574f0e74e3b90d4bda847aad753c4bdf53
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-12-31 18:20:54 +0000

    Time: 171 ms (15.74%) | Memory: 18.6 MB (5.32%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n = len(moveTime)
+        m = len(moveTime[0])
+        heap = [(0, (0,0))]
+        visited = set()
+        sol = -1
+
+        def getChildren(x,y):
+            coor = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+            sol = []
+            for co in coor:
+                if co[0] >= 0 and co[0] < n and co[1] >= 0 and co[1] < m and (co not in visited):
+                    sol.append(co)
+            return sol
+
+        while heap:
+            (dist, (x,y)) = heapq.heappop(heap)
+            if (x,y) in visited: continue
+            if x == n-1 and y == m-1:
+                sol = dist
+            visited.add((x,y))
+            for cx,cy in getChildren(x, y):
+                heapq.heappush(heap, (max(dist+1, moveTime[cx][cy] + 1), (cx,cy)))
+        
+        return sol
