5d1ff755d6fd0c3695f60d1cdf15eca9e43e3016
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2024-12-31 14:15:37 +0000

    Time: 94 ms (99.15%) | Memory: 62.9 MB (47.61%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -1,29 +1,22 @@
 class Solution:
-    def helper(self, nums: List[int], queries: List[List[int]], k:int):
-        n = len(nums)
-        freq = [0] * n
-        for query in queries[:k]:
-            freq[query[0]] += query[2]
-            if query[1] < n-1:
-                freq[query[1] + 1] -= query[2]
-        
-        cur_freq = 0
-        for i in range(n):
-            cur_freq += freq[i]
-            if cur_freq < nums[i]:
-                return False
-        return True
-    
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        n = len(queries)
-        low = 0
-        high = n
-        sol = n+1
-        while(low <= high):
-            mid = low + (high - low)//2
-            if(self.helper(nums, queries, mid)):
-                sol = min(mid, sol)
-                high = mid - 1
-            else:
-                low = mid + 1
-        return sol if sol < n+1 else -1
+        n = len(nums)
+        m = len(queries)
+        i = 0
+        j = 0
+        freqs = [0] * (n+1)
+        freq = 0
+        while(i < n):
+            freq += freqs[i]
+            while(freq < nums[i]):
+                if j >= m:
+                    return -1
+                query = queries[j]
+                if query[0] > i:
+                    freqs[query[0]] += query[2]
+                elif query[0] <=i and query[1] >=i:
+                    freq += query[2]
+                freqs[query[1] + 1] -= query[2]
+                j+=1
+            i+= 1
+        return j if i == n else -1
