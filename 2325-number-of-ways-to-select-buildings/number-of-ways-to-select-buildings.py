8054eef78270e7f2ff040b029c1774328cf5315e
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-02-14 05:10:42 +0000

    Time: 206 ms (97.04%) | Memory: 18.1 MB (80.56%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.py
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.py
@@ -1,22 +1,21 @@
 class Solution:
     def numberOfWays(self, s: str) -> int:
-        n = len(s)
-        dp:List[List[List[int]]] = [[[-1] * 4 for _ in range(2)] for _ in range(n)]
+        no_0 = 0
+        no_1 = 0
+        no_01 = 0
+        no_10 = 0
+        no_101 = 0
+        no_010 = 0
 
-        def recur(req:int, index:int, prev:str) -> int:
-            if(req == 0):
-                return 1
-            if(index >= n):
-                return 0
-            if dp[index][int(prev)][req] != -1:
-                return dp[index][int(prev)][req]
-            next_index = index
-            sol = 0
-            while(next_index < n and s[next_index] == s[index]):
-                next_index += 1
-            sol += recur(req, next_index, prev)
-            if prev != s[index]:
-                sol += (next_index - index) * recur(req-1, next_index, s[index])
-            dp[index][int(prev)][req] = sol
-            return sol
-        return (recur(3, 0, '0') + recur(3, 0, '1'))
+        for c in s:
+            if c == '0':
+                no_0 = no_0 + 1
+                no_10 += no_1
+                no_010 += no_01
+            else:
+                no_1 = no_1 + 1
+                no_01 += no_0
+                no_101 += no_10
+        return no_101 + no_010
+
+        
