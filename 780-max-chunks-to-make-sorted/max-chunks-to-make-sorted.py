9d79a7b4e2e7971f744cb7c7cba21c91396a47f2
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-02-15 00:34:47 +0000

    Time: 0 ms (100.00%) | Memory: 17.7 MB (77.49%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
@@ -1,14 +1,31 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         n = len(arr)
-        num_max = -1
-        num_chunk = 0
-
+        sor_arr = arr.copy()
+        sor_arr.sort()
+        num_chunks = 0
+        num_freqs_arr = defaultdict(lambda: 0)
+        num_freqs_sor = defaultdict(lambda: 0)
+        diff = set()
         for i in range(n):
-            num = arr[i]
-            num_max = max(num, num_max)
+            num_freqs_arr[arr[i]] += 1
+            num_freqs_sor[sor_arr[i]] += 1
 
-            if num_max == i:
-                num_chunk += 1
-        
-        return num_chunk
+            if num_freqs_arr[arr[i]] == num_freqs_sor[arr[i]]:
+                if arr[i] in diff:
+                    diff.remove(arr[i])
+            else:
+                diff.add(arr[i])
+
+            if num_freqs_arr[sor_arr[i]] == num_freqs_sor[sor_arr[i]]:
+                if sor_arr[i] in diff:
+                    diff.remove(sor_arr[i])
+            else:
+                diff.add(sor_arr[i])
+            
+            if len(diff) == 0:
+                num_chunks += 1
+
+
+        return num_chunks
+
