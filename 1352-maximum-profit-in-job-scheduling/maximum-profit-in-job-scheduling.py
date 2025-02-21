198eb189259dbb9f0d6e8342a70c2ebaa696eae7
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-02-21 06:22:15 +0000

    Time: 327 ms (25.57%) | Memory: 39.9 MB (43.22%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,23 +1,34 @@
 class Solution:
+    def bs(self, endtime:List[int], starttime:int):
+        n = len(endtime)
+        low = 0
+        high = n-1
+        sol = -1
+        while(low <= high):
+            mid = low + (high-low)//2
+            if endtime[mid] <= starttime:
+                sol = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return sol
+                
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         n = len(startTime)
-        sorted_arrays = sorted(zip(endTime, startTime, profit))
-        endTime, startTime, profit = map(list, zip(*sorted_arrays))
+        # sorting step
+        paired_list = zip(endTime, startTime, profit)
+        sorted_pairs = sorted(paired_list)
+        sorted_arr1, sorted_arr2, sorted_arr3 = zip(*sorted_pairs)
+        endTime, startTime, profit = list(sorted_arr1), list(sorted_arr2), list(sorted_arr3)
 
-        max_possible = {} # 1-indexed
-        for i in range(n-1, -1, -1):
-            index = bisect.bisect_right(endTime, startTime[i])
-            if index < 0 or index >= n:
-                index = -1
-            elif (endTime[index] != startTime[i]):
-                index = index - 1
+        #bs
+        pair_index = {}
+        for i in range(n):
+            pair_index[i] = self.bs(endTime, startTime[i])
 
-            max_possible[i+1] = index + 1 # 1 indexed
-
-        dp = [0] * (n+1) # 1 indexed
-
-        for i in range(0, n):
-            dp[i+1] = profit[i] + dp[max_possible[i+1]]
-            dp[i+1] = max(dp[i+1], dp[i])
-        
-        return dp[n]
+        # dp
+        dp = [0] * n 
+        dp[0] = profit[0]
+        for i in range(1,n):
+            dp[i] = max(dp[i-1], profit[i] + (dp[pair_index[i]] if pair_index[i] != -1 else 0))
+        return dp[n-1]
