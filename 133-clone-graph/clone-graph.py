f1f3db4bcc6e1311e00de0c3da93b1bee9e65082
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-02-25 21:01:56 +0000

    Time: 41 ms (73.10%) | Memory: 18.2 MB (57.12%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,15 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
 class Solution:
-    def __init__(self):
-        self.visited:Dict[int, 'Node'] = dict()
-    
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if node == None:
-            return None
-        if node.val in self.visited:
-            return self.visited[node.val]
-        node_clone = Node(node.val)
-        self.visited[node.val] = node_clone
-        for neighbour in node.neighbors:
-            neighbour_clone = self.cloneGraph(neighbour)
-            node_clone.neighbors.append(neighbour_clone)
-        return node_clone
+        og_clone = defaultdict(lambda: None)
+        def dfs(node: Optional['Node']):
+            if node == None:
+                return None
+            if og_clone[node] != None:
+                return og_clone[node]
+            new_node = Node(node.val)
+            og_clone[node] = new_node
+            children = []
+            for child in node.neighbors:
+                child_clone = dfs(child)
+                children.append(child_clone)
+            new_node.neighbors = children
+            return new_node
+        return dfs(node)
