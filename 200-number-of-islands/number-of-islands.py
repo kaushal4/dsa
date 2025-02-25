990d1dd7731395af256d5e09e109d6fd33184796
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-02-25 20:50:27 +0000

    Time: 274 ms (30.68%) | Memory: 28.2 MB (11.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,22 @@
 class Solution:
-    def dfs(self, grid: List[List[str]], i: int, j:int, visited:Set[Tuple[int]]):
-        m = len(grid)
-        n = len(grid[0])
-        if i < 0 or i >= m or j < 0 or j >= n or (i,j) in visited or grid[i][j] == "0":
-            return
-        visited.add((i,j))
-        self.dfs(grid, i+1, j, visited)
-        self.dfs(grid, i, j+1, visited)
-        self.dfs(grid, i-1, j, visited)
-        self.dfs(grid, i, j-1, visited)
-
-
     def numIslands(self, grid: List[List[str]]) -> int:
         visited:Set[Tuple[int]] = set()
         m = len(grid)
         n = len(grid[0])
-        count = 0
+        def dfs(x: int, y:int):
+            if(x >= m or x < 0 or y >= n or y < 0 or ((x,y) in visited) or grid[x][y] == "0"):
+                return
+            visited.add((x,y))
+            move = [(1,0), (-1,0), (0,1), (0,-1)]
+            for (dx, dy) in move:
+                child_x = x + dx
+                child_y = y + dy
+                dfs(child_x, child_y)
+
+        solution = 0
         for i in range(m):
             for j in range(n):
-                if (i,j) not in visited and grid[i][j] != "0":
-                    count += 1
-                    self.dfs(grid, i, j, visited)
-        return count
+                if (i,j) not in visited and grid[i][j] == "1":
+                    solution += 1
+                    dfs(i, j)
+        return solution
