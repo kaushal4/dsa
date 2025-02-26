e9da9e47380d8121241c5ad857c417e70c6d2f72
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-02-26 23:20:00 +0000

    Time: 38 ms (89.09%) | Memory: 18.1 MB (44.73%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -2,14 +2,16 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         n = len(s1)
         m = len(s2)
-        dp = [[-1] * (m + 1) for _ in range(n + 1)]
-        dp[0][0] = 1
-        if(n + m != len(s3)):
+        t = len(s3)
+        if n + m != t:
             return False
-        for i in range(0, m+1):
-            for j in range(0, n+1):
-                if i-1 >= 0 and s3[j+i - 1] == s2[i-1] and dp[j][i-1] == 1:
-                    dp[j][i] = 1
-                if j-1 >= 0 and s3[j+i - 1] == s1[j-1] and dp[j-1][i] == 1: 
-                    dp[j][i] = 1
-        return True if dp[n][m]==1 else False
+        @cache
+        def recur(i: int, j:int):
+            if i == -1 and j == -1:
+                return True
+            canForm = False
+            canForm = canForm or (i != -1 and s1[i] == s3[i+j +1] and recur(i-1, j))
+            canForm = canForm or (j != -1 and s2[j] == s3[i+j +1] and recur(i, j-1))
+            return canForm
+
+        return recur(n-1, m-1)
