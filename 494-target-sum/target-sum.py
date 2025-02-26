1586f3f6748acd40f59333d47604488eb2512629
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-02-26 23:05:47 +0000

    Time: 147 ms (57.62%) | Memory: 45.3 MB (7.31%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,28 +1,14 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        agg = sum(nums)
-        upper = agg
-        lower = -agg        
-        if target > upper or target < lower:
-            return 0
-        
-        # check out the indexing
-        dp = {}
-        dp[0] = 1
-
-        for i in range(0, len(nums)):
-            dp_copy = {}
-            for j in range(lower, upper + 1):
-                if j - nums[i] >= lower:
-                    if j-nums[i] in dp:
-                        if j not in dp_copy:
-                            dp_copy[j] = 0
-                        dp_copy[j] += dp[j-nums[i]]
-                if j + nums[i] <= upper:
-                    if j+nums[i] in dp:
-                        if j not in dp_copy:
-                            dp_copy[j] = 0
-                        dp_copy[j] += dp[j+nums[i]]
-            dp = dp_copy
-        return dp[target] if target in dp else 0
+        @cache
+        def recur(i:int, amt: int):
+            if (i == n and amt == target):
+                return 1
+            if i >= n:
+                return 0
+            count = 0
+            count += recur(i+1, amt + nums[i])
+            count += recur(i+1, amt - nums[i])
+            return count
+        return recur(0, 0)
