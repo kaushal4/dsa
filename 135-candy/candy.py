659190776c7d313d6ac55db95f6b5236938b402c
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-03-09 03:57:24 +0000

    Time: 23 ms (24.44%) | Memory: 20 MB (19.41%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -3,16 +3,19 @@ class Solution:
         n = len(ratings)
         left = [1] * n
         right = [1] * n
-        sol = 0
-        for i in range(1, n):
+
+        for i in range(1,n):
             if ratings[i] > ratings[i-1]:
-                left[i] = left[i - 1] + 1
+                left[i] = left[i-1] + 1
+
+
+        
         for i in range(n-2, -1, -1):
             if ratings[i] > ratings[i+1]:
-                right[i] = right[i + 1] + 1
-    
-        for i in range(0, n):
-            sol += max(right[i], left[i])
+                right[i] = right[i+1] + 1
+
+        ans = 0
+        for i in range(n):
+            ans += max(left[i], right[i])
         
-        return sol
-        
+        return ans
