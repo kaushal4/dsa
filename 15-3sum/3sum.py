3f4e9354fb336577688c919fb38ead3dcb638282
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-08-16 12:29:13 -0700

    Time: 1407 ms (10.79%) | Memory: 20.6 MB (69.55%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,26 +1,46 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
-        sol = list()
-        
-        for first in range(0, n-2):
-            # condition - 1 to avoid duplicates
-            if first > 0 and nums[first] == nums[first-1]:
-                continue
-            second = first + 1
-            third = n - 1
+        size = len(nums)
+        def has_three_zeroes():
+            count = 0
+            max_count = 0
+            for num in nums:
+                if num == 0:
+                    count += 1
+                else: count = 0
+                max_count = max(count, max_count)
+            if max_count > 2:
+                return True
+            return False
 
-            while (second < third):
-                sum = nums[first] + nums[second] + nums[third]
-                if sum == 0:
-                    sol.append((nums[first], nums[second], nums[third]))
-                    # condition - 2 to avoid duplicates
-                    second += 1
-                    while second < n-1 and nums[second] == nums[second - 1]:
-                        second += 1
-                elif (sum < 0):
-                    second += 1
+        def findRight(i:int):
+            while i != (size -1) and nums[i] == nums[i+1]:
+                i += 1
+            return i
+
+        def findLeft(i:int):
+            while i != 0 and nums[i] == nums[i-1] and nums[i]:
+                i -= 1
+            return i
+        
+        left_anchor = 0
+        ans: List[List[int]] = []
+        while left_anchor < size-2:
+            low = findRight(left_anchor)
+            low = low if low != left_anchor else left_anchor + 1
+            high = size - 1
+            while(low < high):
+                if nums[left_anchor] + nums[low] + nums[high] == 0:
+                    ans.append([nums[left_anchor], nums[low], nums[high]])
+                
+                if nums[left_anchor] + nums[low] + nums[high] < 0:
+                    low = findRight(low) + 1
                 else:
-                    third -= 1
-        return sol
+                    high = findLeft(high) - 1
+            left_anchor = findRight(left_anchor)+1
+        
+        if has_three_zeroes():
+            ans.append([0,0,0])
+
+        return ans
