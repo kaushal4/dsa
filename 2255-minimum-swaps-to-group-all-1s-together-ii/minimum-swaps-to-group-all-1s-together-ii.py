cc582e4b748ec0c5c6f54606028c05bf0d413e23
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-08-17 16:21:03 -0700

    Time: 179 ms (33.25%) | Memory: 22.3 MB (44.92%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,34 +1,20 @@
 class Solution:
-    def maxArrayOf(self, nums, n):
-        cur_sum = 0
-        low = 0
-        high = n - 1
-        cur_sum = sum(nums[low: high + 1])
-        max_sum = cur_sum
-        while high < len(nums) - 1 and low < len(nums):
-            cur_sum = cur_sum - nums[low] + nums[high + 1]
-            high += 1
-            low += 1
-            max_sum = max(max_sum, cur_sum)
-        return max_sum
-
-    def minArrayOf(self, nums, n):
-        cur_sum = 0
-        low = 0
-        high = n - 1
-        cur_sum = sum(nums[low: high + 1])
-        min_sum = cur_sum
-        while high < len(nums) - 1:
-            cur_sum = cur_sum - nums[low] + nums[high + 1]
-            high += 1
-            low += 1
-            min_sum = min(min_sum, cur_sum)
-        return min_sum
-
-
     def minSwaps(self, nums: List[int]) -> int:
-        count_1 = 0
-        for num in nums:
-            if num == 1:
-                count_1 += 1
-        return min(count_1 - self.maxArrayOf(nums, count_1),self.minArrayOf(nums, len(nums) - count_1))
+        count_one = sum(nums)
+        og_size = len(nums)
+
+        for i in range(og_size):
+            nums.append(nums[i])
+        size = len(nums)
+        
+        low = 0
+        high = count_one - 1
+        sub_count = sum(nums[low: high + 1])
+        sol = (count_one - sub_count)
+        while high < size:
+            sol = min(count_one - sub_count, sol)
+            high += 1
+            if high < size: sub_count = sub_count + nums[high]
+            if low < size: sub_count -= nums[low]
+            low += 1 
+        return sol
