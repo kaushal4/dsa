d60932750f89011afbcd2bb4732c2990a912ad66
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-09-01 19:56:56 -0400

    Time: 316 ms (5.02%) | Memory: 28.5 MB (51.25%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return false;
+        int min_value = *min_element(nums.begin(), nums.end());
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
+        if(total_sum%2 != 0) return false;
+        int desired_sum = total_sum/2;
+
+        vector<vector<bool>> dp(desired_sum - min_value + 1, vector<bool> (n+1, false));
+
+        for(int i = min_value; i <= desired_sum; i++) {
+            for(int j = 1; j < n+1; j++) {
+                dp[i-min_value][j] = dp[i-min_value][j] || dp[i-min_value][j-1];
+                if(i - nums[j-1] < 0) continue;
+                if(i - nums[j-1] == 0) dp[i - min_value][j] = true;
+                if(i - nums[j-1] >= min_value) dp[i - min_value][j] = (dp[i - min_value][j] || dp[i - nums[j-1] - min_value][j-1]);
+            }
+        }
+
+        bool sol = false;
+        for(int j = 0; j < n+1; j++) {
+            sol = sol || dp[desired_sum-min_value][j];
+        }
+        return sol;
+    }
+};
