d2ba6561fa5064ad301704b2f993e5dee5d2cb9d
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-09-01 20:06:10 -0400

    Time: 127 ms (62.25%) | Memory: 13.2 MB (98.16%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -8,26 +8,21 @@ public:
         if(total_sum%2 != 0) return false;
         int desired_sum = total_sum/2;
 
-        vector<vector<bool>> dp(desired_sum - min_value + 1, vector<bool> (n+1, false));
+        vector<bool> dp(desired_sum - min_value + 1, false);
 
         for (int j = 1; j < n + 1; j++)
         {
-            for (int i = min_value; i <= desired_sum; i++)
+            for (int i = desired_sum; i >= min_value; i--)
             {
-                dp[i - min_value][j] = dp[i - min_value][j] || dp[i - min_value][j - 1];
                 if (i - nums[j - 1] < 0)
                     continue;
                 if (i - nums[j - 1] == 0)
-                    dp[i - min_value][j] = true;
+                    dp[i - min_value] = true;
                 if (i - nums[j - 1] >= min_value)
-                    dp[i - min_value][j] = (dp[i - min_value][j] || dp[i - nums[j - 1] - min_value][j - 1]);
+                    dp[i - min_value] = (dp[i - min_value] || dp[i - nums[j - 1] - min_value]);
             }
         }
 
-        bool sol = false;
-        for(int j = 0; j < n+1; j++) {
-            sol = sol || dp[desired_sum-min_value][j];
-        }
-        return sol;
+        return dp[desired_sum - min_value];
     }
 };
