ec370df429d0e8d0b1ea7cb72324cb53ed83181f
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-09-05 00:18:31 -0400

    Time: 0 ms (100.00%) | Memory: 14.5 MB (17.80%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -2,77 +2,60 @@ class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int max_node = 0;
-        for (const auto &edge : edges) {
-            max_node = max(max_node, max(edge[0], edge[1]));
+        unordered_set<int> visited;
+        for(const auto& edge:edges){
+            max_node = max(max_node, edge[0]);
+            max_node = max(max_node, edge[1]);
         }
-        vector<vector<int>> graph(max_node+1);
-        for(const auto& edge: edges){
+
+        vector<vector<int>> graph(max_node + 1, vector<int>());
+        for(const auto& edge:edges){
             graph[edge[0]].push_back(edge[1]);
             graph[edge[1]].push_back(edge[0]);
         }
         vector<int> parent(max_node+1, -1);
-        queue<int> q;
-        q.push(1);
-
         int u = -1;
-        int v = -1; 
-        parent[1] = 1;
+        int v = -1;
+        dfs(graph, visited, parent, 1, -1, &u, &v);
 
-        while(!q.empty() && u == -1){
-            int cur_node = q.front(); q.pop();
-            for(const auto& child_node: graph[cur_node]) {
-                if(parent[child_node] != -1 && parent[cur_node] != child_node) {
-                    u = child_node;
-                    v = cur_node;
-                    break;
-                }
-                if(parent[child_node] == -1){
-                    parent[child_node] = cur_node;
-                    q.push(child_node);
-                }
-            }
-        }
-
-        parent[1] = -1;
-
-        vector<int> cycle_elements;
-        unordered_set<int> v_elements;
-        auto pair_hash = [](const pair<int, int>& p) {
+        const auto pair_hash = [](const pair<int, int> p) {
             return hash<int>{}(p.first) ^ hash<int>{}(p.second);
         };
-        int head = v;
-        while(head != -1){
-            v_elements.insert(head);
+        unordered_set<pair<int, int>, decltype(pair_hash)> cycle_nodes;
+        int head = u;
+        while(head != v){
+            cycle_nodes.insert({head, parent[head]});
+            cycle_nodes.insert({parent[head], head});
             head = parent[head];
         }
-        head = u;
-        while(!v_elements.contains(head)){
-            head = parent[head];
-        }
-        int common_point = head;
-        unordered_set<pair<int, int>, decltype(pair_hash)> cycle_edges;
-        head = v;
-        while(head != common_point){
-            cycle_edges.insert({head, parent[head]});
-            cycle_edges.insert({parent[head], head});
-            head = parent[head];
-        }
-        head = u;
-        while(head != common_point){
-            cycle_edges.insert({head, parent[head]});
-            cycle_edges.insert({parent[head], head});
-            head = parent[head];
-        }
-
-        cycle_edges.insert({u, v});
-        cycle_edges.insert({v, u});
+        cycle_nodes.insert({u, v});
+        cycle_nodes.insert({v, u});
 
         reverse(edges.begin(), edges.end());
-
-        for(auto& edge: edges){
-            pair<int, int> edge_pair = pair(edge[0], edge[1]);
-            if(cycle_edges.contains(edge_pair)){return vector<int>{edge[0], edge[1]};};
+        
+        for(const auto& edge: edges){
+            if(cycle_nodes.contains({edge[0], edge[1]})){
+                return vector<int> {edge[0], edge[1]};
+            }
         }
         return vector<int>();
+    }
+
+    void dfs(vector<vector<int>>& graph,unordered_set<int>& visited,vector<int>& p, int node, int parent, int * u, int * v) {
+        if(*u != -1) return; 
+        if(visited.contains(node)){
+            *v = node;
+            return;
+        }
+        visited.insert(node);
+        p[node] = parent;
+        for(int child_node: graph[node]){
+            if(child_node == parent) continue;
+            dfs(graph, visited,p, child_node, node, u, v);
+            if(*v != -1 && *u == -1){
+                *u = node;
+                return;
+            }
+        }
     }
 };
