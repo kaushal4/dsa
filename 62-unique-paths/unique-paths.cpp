d7078d8671e7676c38c422ec2655032b5acbdba5
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-09-06 16:42:04 -0400

    Time: 0 ms (100.00%) | Memory: 7.7 MB (96.76%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> dp (n, 0);
-        dp[0] = 1;
-
-        for(int i = 0; i < m; i++){
-            for(int j = 1; j < n; j++){
-                dp[j] = dp[j-1] + dp[j];
-            }
+        // We need (m-1) down moves and (n-1) right moves
+        // Total moves = (m-1) + (n-1) = m + n - 2
+        // Answer = C(m+n-2, m-1) = C(m+n-2, n-1)
+        
+        int totalMoves = m + n - 2;
+        int downMoves = m - 1;
+        int rightMoves = n - 1;
+        
+        // Choose the smaller of the two to minimize computation
+        int r = min(downMoves, rightMoves);
+        
+        // Calculate C(totalMoves, r) = totalMoves! / (r! * (totalMoves-r)!)
+        // We'll compute this iteratively to avoid overflow
+        long long result = 1;
+        
+        for (int i = 0; i < r; i++) {
+            result = result * (totalMoves - i) / (i + 1);
         }
-        return dp[n-1];
+        
+        return (int)result;
     }
 };
