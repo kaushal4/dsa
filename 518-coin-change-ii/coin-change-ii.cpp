461063605a43b19ba2763de8254b4902855f0ec7
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-09-07 00:32:50 -0400

    Time: 3 ms (94.80%) | Memory: 10.9 MB (76.83%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        using u128 = unsigned __int128;
         if (amount == 0) return 1;
         if (coins.empty()) return 0;
         
-        // Early impossibility detection based on parity
-        bool hasOdd = false;
-        for (int coin : coins) {
-            if (coin > 0 && coin % 2 == 1) {
-                hasOdd = true;
-                break;
-            }
-        }
-        
-        // If amount is odd but all valid coins are even, impossible
-        if (amount % 2 == 1 && !hasOdd) {
-            return 0;
-        }
-        
-        vector<int> dp(amount + 1, 0);
+        vector<u128> dp(amount + 1, 0);
         dp[0] = 1;
         
         for (int coin : coins) {
