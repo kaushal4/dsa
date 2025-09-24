d5f75a67f56c34f01447842a721ca8885f6d52ad
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-09-24 13:16:33 -0400

    Time: 23 ms (33.72%) | Memory: 95.6 MB (94.14%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int findMin(int target){
-        int ans = INT_MAX;
-        int i = 0;
-        while(target - (3 * i) >= 0){
-            int curNum = target - (3*i);
-            if(curNum % 2 == 0){
-                ans = min(ans, i + curNum / 2);
+    int minOperations(std::vector<int>& nums) {
+        std::unordered_map<int, int> counts;
+        for (int num : nums) {
+            counts[num]++;
+        }
+
+        int total_ops = 0;
+        for (auto const& [num, count] : counts) {
+            // It's impossible to clear a single element
+            if (count == 1) {
+                return -1;
             }
-            i++;
+            
+            // Use the O(1) mathematical formula
+            total_ops += (count + 2) / 3;
         }
-        return ans == INT_MAX?-1:ans;
-    }
-    int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> count;
-        int ans = 0;
-        for(int num : nums){
-            count[num]+= 1;
-        }
-        for(const auto[key, value] : count){
-            int curCount = findMin(value);
-            if(curCount == -1) return -1;
-            ans += curCount;
-        }
-        return ans;
+        
+        return total_ops;
     }
 };
