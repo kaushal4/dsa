631ade9099ec109e09499ff8384b7f008eddff03
kaushal damania <53399984+kaushal4@users.noreply.github.com>
2025-10-08 13:27:32 -0400

    Time: 388 ms (5.16%) | Memory: 56.7 MB (67.70%) - LeetSync

---
+++ 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minSizeSubarray(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = -1;
+        long long high = -1;
+        long long cur_sum = 0;
+        long long base_sum = accumulate(nums.begin(), nums.end(),(long long) 0);
+        long long sol = __LONG_LONG_MAX__;
+
+        while(low < n){
+            if(cur_sum < target){
+                if(cur_sum + base_sum <= target){
+                    cur_sum += base_sum;
+                    high += n; 
+                } else {
+                    high += 1;
+                    cur_sum += nums[high%n];
+                }
+            } else {
+                if(low != -1){
+                    cur_sum -= nums[low];
+                }
+                low += 1;
+            }
+            if(cur_sum == target){
+                if(low == -1){
+                    sol = min(sol, (long long)high - low);
+                } else {
+                    sol = min(sol, (long long)high - low + 1);
+                }
+            }
+        }
+        return sol == INT_MAX?-1:sol;
+    }
+};
